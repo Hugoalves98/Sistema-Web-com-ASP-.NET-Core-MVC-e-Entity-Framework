330e96fa054d0012bfedca807d5eb6db4fcd238e
Hugo Rocha <hugorochalves@gmail.com>
2022-09-28 16:38:46 -0300

    Atualizar vendedor e servico customizado de excecao

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -2,6 +2,7 @@
 using SaleWebMVC.Models;
 using SaleWebMVC.Models.ViewModel;
 using SaleWebMVC.Services;
+using SaleWebMVC.Services.Exceptions;
 
 namespace SaleWebMVC.Controllers
 {
@@ -68,6 +69,45 @@ namespace SaleWebMVC.Controllers
                 return NotFound();
             }
             return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            List<Department> departments = _departmentService.FindAll();    
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments};
+            return View(viewModel);
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try 
+            { 
+            _sellerService.Update(seller);
+            return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
         }
     }
 }
---
+++ Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace SaleWebMVC.Services.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message) :  base(message)
+        {
+
+        }
+    }
+}
