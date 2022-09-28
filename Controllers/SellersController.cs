fa714a3a5f25f982de854cb6eb7519aa36b07bee
Hugo Rocha <hugorochalves@gmail.com>
2022-09-27 22:56:50 -0300

    Simples criaçao de form

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SaleWebMVC.Models;
 using SaleWebMVC.Services;
 
 namespace SaleWebMVC.Controllers
@@ -17,6 +18,18 @@ namespace SaleWebMVC.Controllers
             var list = _sellerService.FindAll();
 
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
