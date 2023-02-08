2f409d7d78258e7583c8120171a604662ae82e84
Hugo Rocha <hugorochalves@gmail.com>
2023-02-07 22:57:38 -0300

    Novas atts

--- Services/SalesRecordService.cs
+++ Services/SalesRecordService.cs
@@ -30,7 +30,7 @@ namespace SaleWebMVC.Services
                 .OrderByDescending(x => x.Date)
                 .ToListAsync();
         }
-        public async Task<List<IGrouping<Department?,SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        public async Task<List<IGrouping<Department,SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
         {
             var result = from obj in _context.SalesRecord select obj;
             if (minDate.HasValue)
